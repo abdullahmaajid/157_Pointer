583e6573ffe2db5c3a55e86e070ef039281ee81b
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-05-08 14:55:09 +0700

    int main

--- 157_Pointer/157_Pointer.cpp
+++ 157_Pointer/157_Pointer.cpp
@@ -3,12 +3,20 @@ using namespace std;
 
 class mahasiswa {
 public:
-	int	nim;
+	int nim;
 	void showNim();													//Deklarasi Method
 };
 
 void mahasiswa::showNim()	{										//implementasi method diluar class
 	cout << "No Induk = " << nim << endl;
+}
+
+int main()
+{
+	mahasiswa mhs{ 1 }; //object mhs
+	mhs.showNim(); // Member acces operator
+
+	mahasiswa& ref = mhs; // Pointer Reference refMhs
 }
 
 
